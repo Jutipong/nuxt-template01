dface556ece148710e006cfd3daea91e003ddd25
ohm <techcom.ec20@gmail.com>
2020-08-04 17:26:22 +0700

    Config Update.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list shaped dense>
         <template v-for="item in items">
           <v-row v-if="item.heading" :key="item.heading" align="center"></v-row>
@@ -22,12 +16,8 @@
                 <v-list-item-title>{{ item.text }}</v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item
-              v-for="(child, i) in item.children"
-              :key="i"
-              link
-              :to="child.to"
-            >
+
+            <v-list-item v-for="(child, i) in item.children" :key="i" link :to="child.to">
               <v-list-item-action v-if="child.icon" class="ml-2">
                 <v-icon>{{ child.icon }}</v-icon>
               </v-list-item-action>
@@ -49,15 +39,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      small
-      :clipped-left="clipped"
-      fixed
-      app
-      color="blue darken-3"
-      dark
-      dense
-    >
+    <v-app-bar small :clipped-left="clipped" fixed app color="blue darken-3" dark dense>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-spacer />
     </v-app-bar>
@@ -125,28 +107,28 @@ export default {
       right: true,
       // rightDrawer: false,
       title: 'Vuetify.js',
-    }
+    };
   },
   mounted() {
     this.items.forEach((r) => {
       if (r.children != null) {
         let obj = r.children.find((c) => {
-          return c.to.toUpperCase() == $nuxt.$route.name.toUpperCase()
-        })
+          return c.to.toUpperCase() == $nuxt.$route.name.toUpperCase();
+        });
 
         if (obj != undefined && obj != null) {
           let rootmenu = this.items.find((r) => {
-            return r.index === obj.index_root
-          })
+            return r.index === obj.index_root;
+          });
 
           if (rootmenu != undefined && rootmenu != null) {
-            rootmenu.model = true
+            rootmenu.model = true;
           }
         }
       }
-    })
+    });
   },
-}
+};
 </script>
 
 <style>
